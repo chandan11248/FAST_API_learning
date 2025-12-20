777b64f6c385ce1acb7abcd2201768ba65060817
chandan shah <letschandansah@gmail.com>
2025-12-20 18:53:06 +0545

    post request

--- 1.py
+++ 1.py
@@ -1,6 +1,52 @@
 from fastapi import FastAPI,Path,HTTPException,Query
+from typing import Annotated,Literal
+from pydantic import field_validator,Field,computed_field,BaseModel
+from fastapi.responses import JSONResponse
 import json
 app=FastAPI()
+
+def load():   
+    with open ("patients.json") as f:
+            data=json.load(f)
+            return data
+    
+def save(data):
+    with open ("patients.py","w") as f:
+        json.dump(data,f)
+
+
+ 
+class Patient(BaseModel):
+
+    id: Annotated[str, Field(..., description='ID of the patient', examples=['P001'])]
+    name: Annotated[str, Field(..., description='Name of the patient')]
+    city: Annotated[str, Field(..., description='City where the patient is living')]
+    age: Annotated[int, Field(..., gt=0, lt=120, description='Age of the patient')]
+    gender: Annotated[Literal['male', 'female', 'others'], Field(..., description='Gender of the patient')]
+    height: Annotated[float, Field(..., gt=0, description='Height of the patient in mtrs')]
+    weight: Annotated[float, Field(..., gt=0, description='Weight of the patient in kgs')]
+
+    @computed_field
+    @property
+    def bmi(self) -> float:
+        bmi = round(self.weight/(self.height**2),2)
+        return bmi
+    
+    @computed_field
+    @property
+    def verdict(self) -> str:
+
+        if self.bmi < 18.5:
+            return 'Underweight'
+        elif self.bmi < 25:
+            return 'Normal'
+        elif self.bmi < 30:
+            return 'Normal'
+        else:
+            return 'Obese'
+
+
+
 @app.get('/')
 def fun():
     return {'messages':"hello"}
@@ -8,10 +54,7 @@ def fun():
 def secret():
     return {"aiMessage":"hi,There!!",
             "humanMesssage":"hello ai, how are you?"}    
-def load():   
-    with open ("patients.json") as f:
-            data=json.load(f)
-            return data
+
 
 @app.get("/view")
 def view():
@@ -50,4 +93,13 @@ def sort_patient(sort_by:str=Query(...,
      return sorted_data
     
 
+@app.post("/create")
+def create_patient(patient:Patient):
+    data=load()
+    if patient.id in data: 
+        raise HTTPException(status_code=400,detail="patient all ready exists")
+    data[patient.id]=patient.model_dump(exclude=['id'])#model_dump makes the pydantic to dictionary 
+    save(data)
+    return JSONResponse(status_code=201,content={"message":"patient created successful"})
+ 
  
